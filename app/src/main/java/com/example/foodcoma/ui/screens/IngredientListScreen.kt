050458e8def58a730bc5e704aca21668b010a796
MattiasOz <mattiasjosefoz@gmail.com>
2024-05-17 23:21:28 +0200

    Minor fix to UI

--- app/src/main/java/com/example/foodcoma/ui/screens/IngredientListScreen.kt
+++ app/src/main/java/com/example/foodcoma/ui/screens/IngredientListScreen.kt
@@ -1,6 +1,7 @@
 package com.example.foodcoma.ui.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -65,7 +66,7 @@ private fun IngredientCard(
     ) {
         Box(
             contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxSize()
         ) {
             Text(ingredient.strIngredient)
         }
