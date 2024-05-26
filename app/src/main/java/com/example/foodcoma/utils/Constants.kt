6c756fe103ab8cbaff6938b66457ddb0feeedf87
MattiasOz <mattiasjosefoz@gmail.com>
2024-05-26 19:36:55 +0200

    Pruned a bit

--- app/src/main/java/com/example/foodcoma/utils/Constants.kt
+++ app/src/main/java/com/example/foodcoma/utils/Constants.kt
@@ -1,6 +1,5 @@
 package com.example.foodcoma.utils
 
-import androidx.compose.ui.unit.dp
 
 object Constants {
     const val SERVER_BASE_URL = "https://www.themealdb.com/api/json/v1/"
@@ -8,7 +7,6 @@ object Constants {
 
     const val SCHEDULED_RELOAD_TAG = "SCHEDULED_RELOAD_TAG"
     const val RELOAD_PAGE_TAG = "RELOAD_PAGE_TAG"
-    const val RELOAD_CATEGORY_TAG = "RELOAD_CATEGORY_TAG"
 
     const val PULL_TO_REFRESH_THRESHOLD = 100
 }
